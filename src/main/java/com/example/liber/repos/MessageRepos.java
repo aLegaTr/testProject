d2bb3079ebe7df93bb7dfd60af0343865aca87a0
CV <37792178+aLegaTr@users.noreply.github.com>
2018-08-22 18:16:44 +0300

    add testSpringBootProject

---
+++ src/main/java/com/example/liber/repos/MessageRepos.java
@@ -0,0 +1,11 @@
+package com.example.liber.repos;
+
+import com.example.liber.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepos extends CrudRepository<Message, Integer> {
+
+    List<Message> findByTag(String tag);
+}
